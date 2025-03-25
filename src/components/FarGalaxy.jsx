dc0e7dd9110f46f4bb7dde95de16a3f6f4ef3126
natal <natalie77335@gmail.com>
2025-03-25 19:28:50 +0100

    Complete navigation

--- src/components/FarGalaxy.jsx
+++ src/components/FarGalaxy.jsx
@@ -1,9 +1,21 @@
-import {openingCrawl} from "../utils/constants.js";
+import {useEffect, useState} from "react";
+import {base_url} from "../utils/constants.js";
 
 const FarGalaxy = () => {
+    const [openingCrawl, setOpenningCrawl] = useState('Loading...');
+
+    useEffect(() => {
+        const episode = Math.floor(Math.random() * 6 + 1);
+        fetch(`${base_url}/v1/films/3`)
+            .then(response => response.json())
+            .then(data => setOpenningCrawl(data.opening_crawl))
+
+
+    }, [])
+
     return (
         <p className="farGalaxy">
-                {openingCrawl}
+            {openingCrawl}
         </p>
     );
 };
---
+++ src/components/StarWars.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StarWars = () => {
+    return (
+        <div className={'farGalaxy'}>
+            <starWarsInfo/>
+
+        </div>
+    );
+};
+
+export default StarWars;
